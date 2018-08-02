8735751d499eaf01f3d2daa40a7e94d9f730024b
Ever Cifuentes <everbilly35@gmail.com>
2018-08-02 16:03:21 -0600

    [UPDATED] add space in file

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -44,7 +44,7 @@ export class AuthService {
                       .then(response => {
                         return response.json()
                       })
-                      .catch(this.handleError)
+                      .catch(this.handleError) 
   }
 
 }
